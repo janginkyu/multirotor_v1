53739d3bd34d7ff2750c0302368b65127c3540f3
Jang Inkyu <leplusbon@snu.ac.kr>
2019-07-08 16:59:58 +0900

    rev

--- src/gpioPWM/scripts/read.py
+++ src/gpioPWM/scripts/read.py
@@ -59,7 +59,7 @@ def publisher():
             if dataRefresh[pinNum]:
                 pub[pinNum].publish(dTime[pinNum])
                 dataRefresh[pinNum] = False
-                rospy.loginfo(str(dTime[pinNum]))
+                rospy.loginfo((' ' * (dTime[pinNum] / 100)) + '*')
         rate.sleep()
 
 if __name__ == '__main__':
