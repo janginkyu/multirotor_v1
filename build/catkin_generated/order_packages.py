d34b7c7abddba38b759b67d54fcfd17e0efe20e2
Jang Inkyu <leplusbon@snu.ac.kr>
2019-07-08 12:42:55 +0900

    initial commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/janginkyu/rpi_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/janginkyu/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/janginkyu/catkin_ws/devel;/opt/ros/melodic" != "" else []
